2396262166efe065cc7564a7de011553793953bf
MmatheusmMartini13 <matheusmartini13@gmail.com>
2023-01-23 18:11:09 -0300

    API GET

---
+++ index.js
@@ -0,0 +1,25 @@
+function createCountryCard(country){
+  const card = document.createElement('div')
+  card.classList.add('country')
+
+  const countryName = country.name.common
+  const name = document.createElement('h2')
+  name.textContent = countryName
+
+  const flag = document.createElement('img')
+  flag.src = country.flags.svg
+  flag.alt = countryName
+
+  card.append(name, flag)
+  document.getElementById('countries').append(card)
+}
+
+async function getCountries(){
+  const response = await fetch('https://restcountries.com/v3.1/all')
+  const country = await response.json()
+  console.log(country[42])
+
+  country.forEach(createCountryCard);
+}
+
+getCountries()
